59854ca1983a5b352480a52d2fd5750fda0bff0c
AJINKYAVENGURLEKAR <142030961+AJINKYAVENGURLEKAR@users.noreply.github.com>
2025-02-02 16:44:29 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,13 +1,1 @@
-# main.py
-from fastapi import FastAPI
 
-app = FastAPI()
-
-@app.get("/")
-def read_root():
-    return {"message": "Hello, World!"}
-
-{
-  "builds": [{ "src": "main.py", "use": "@vercel/python" }],
-  "routes": [{ "src": "/(.*)", "dest": "main.py" }]
-}
